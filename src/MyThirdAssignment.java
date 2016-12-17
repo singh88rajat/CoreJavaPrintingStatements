0d8b6502a70ce455538b64ce44567c1ad6e243cb
Mukesh Kumar <singh88rajat@gmail.com>
2016-12-17 22:06:49 +0530

    CoreJavaPrintingStatement

---
+++ src/MyThirdAssignment.java
@@ -0,0 +1,15 @@
+public class MyThirdAssignment {
+	 public static void main(String args[]){
+		 System.out.println("Second Assignment for printing Values :");
+		    int a=20;
+	    	int b=10;
+	    	int z= a-- - --a;
+	    	int c=a--;
+	    	int d=a>>2;
+	    	int e=a&b;
+	    	System.out.println("Z Value:" +z);
+	    	System.out.println("C Value:" +c);
+	    	System.out.println("D Value:" +d);
+	    	System.out.println("E Value:" +e);
+	 }
+}
